7a9cb205b0e61a9c125415148e6e519e2fe8ec33
Nabeel Ahmed <nabeelahmed1699@gmail.com>
2022-01-17 20:43:53 +0500

    put the arrays in single script file and change the path of the projects relative the html file not the script itself

--- script/script.js
+++ script/script.js
@@ -36,10 +36,83 @@ links.forEach(link => {
         document.body.style.overflowY = "scroll";
     })
 });
+// ========================= Js Projects =====================================
+const jsProjects = [{
+            id: 1,
+            name: 'Todo list App',
+            img: './images/projects/Todolistapp.png',
+            visitLink: 'https://nabeelahmed1699.github.io/To-do-App/',
+            sourceCode: 'https://github.com/nabeelahmed1699/To-do-App',
+            desc: `The basic function of this app is to create a list of todos. It also contain random quote generator functionality achieved using an api.`
+        },
+        {
+            id: 2,
+            name: 'Calculator App',
+            img: './images/projects/calculatorapp.png',
+            visitLink: 'https://nabeelahmed1699.github.io/Calculator-App/',
+            sourceCode: 'https://github.com/nabeelahmed1699/Calculator-App',
+            desc: `Basic calculator app that can perform basic arithmatic functions.`
+        },
 
+        {
+            id: 3,
+            name: 'Weather App',
+            img: './images/projects/weatherapp.png',
+            visitLink: 'https://nabeelahmed1699.github.io/Weather-App/',
+            sourceCode: 'https://github.com/nabeelahmed1699/Weather-App',
+            desc: `Weather app using openWeather api.It is an open source api. Quite fun project.`
+        },
+        {
+            id: 4,
+            name: 'Event Counter App',
+            img: './images/projects/Eventcounterapp.png',
+            visitLink: 'https://nabeelahmed1699.github.io/event-countdown-app/',
+            sourceCode: 'https://github.com/nabeelahmed1699/event-countdown-app',
+            desc: `This app will count the remaining time in a specified event accur.I've set it to my next birthday!`
+        },
+        {
+            id: 5,
+            name: 'Rock paper scissor Game',
+            img: './images/projects/rockpapergame.png',
+            visitLink: 'https://nabeelahmed1699.github.io/Rock-paper-scissor-game/',
+            sourceCode: 'https://github.com/nabeelahmed1699/Rock-paper-scissor-game',
+            desc: `A fun project trying to built a game.Select one card among three and computer will randomly choose against it.`
+        },
+        {
+            id: 6,
+            name: 'Screen Recorder App',
+            img: './images/projects/screenrecorderapp.png',
+            visitLink: 'https://nabeelahmed1699.github.io/screen-recorder/',
+            sourceCode: 'https://github.com/nabeelahmed1699/screen-recorder',
+            desc: `Screen recorder app. This app only works on desktops not in mobiles!`
+        }
+    ]
+    // ========================= Web Projects ====================================
+const webProjects = [{
+        id: 2,
+        name: 'Space website',
+        desc: 'Space website is a challenge project from frontend mentor.Check the source code in my github repo!',
+        link: 'https://nabeelahmed1699.github.io/space-website/',
+        background: './images/projects/spacewebsite.png'
+    },
+    {
+        id: 3,
+        name: `Franky's website`,
+        desc: `Franky's is my local fast food restaurant.I tried and created a landing page for the restaurant.`,
+        link: 'https://nabeelahmed1699.github.io/Fast-food-website-mockup/',
+        background: './images/projects/frankyswebsite.png'
+    },
+    {
+        id: 4,
+        name: 'Apple website clone',
+        desc: 'I’ve tried and clone the apple website landing not pixel perfect but tried my best to make it same as much as possible, also it is completely responsive. Visit my Github to see the project source code fork it and make it yours.',
+        link: 'https://nabeelahmed1699.github.io/Apple-clone/',
+        background: './images/projects/appleclone.png'
+    }
+];
 // ========================== Projects handling ==============================
-import { webProjects } from "./webProjects.js";
-import { jsProjects } from "./jsProjects.js";
+// import { webProjects } from "./webProjects.js";
+// import { jsProjects } from "./jsProjects.js";
 
 // ===================== Web projects creation =============================
 function createWebProject(project) {
